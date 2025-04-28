7950abbbf20671d2332ce6b14ec189050ba6fc6a
Elizabeth Craig <elcraig@microsoft.com>
2025-04-28 15:11:37 -0700

    naming

--- packages/app/index.js
+++ packages/app/index.js
@@ -1,4 +1,4 @@
-import { a } from '@renamed-dupes-repro/a';
-import { b } from '@renamed-dupes-repro/b';
+import { func as a } from '@renamed-dupes-repro/good';
+import { func as b } from '@renamed-dupes-repro/weird';
 
 console.log(a, b);
